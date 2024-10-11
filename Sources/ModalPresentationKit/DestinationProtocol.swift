1ddeae01cfd7158567cc222d036b3f9591f19e2c
quantumakh <aadzhanow@gmail.com>
2024-10-11 13:35:40 +0200

    Update DestinationProtocol.swift

--- Sources/ModalPresentationKit/DestinationProtocol.swift
+++ Sources/ModalPresentationKit/DestinationProtocol.swift
@@ -5,6 +5,22 @@
 //  Created by Alisher on 09.10.2024.
 //
 
+// DestinationProtocol.swift
+
 import SwiftUI
 
-public typealias DestinationProtocol = View & Identifiable & Hashable
+public protocol BaseDestinationProtocol: View, Identifiable, Hashable {}
+
+public extension BaseDestinationProtocol where Self: Equatable {
+    static func ==(lhs: Self, rhs: Self) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
+public extension BaseDestinationProtocol {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+public typealias DestinationProtocol = BaseDestinationProtocol
