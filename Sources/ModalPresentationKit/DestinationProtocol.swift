0b9465de6a30897f201316e9a5509a952ef01ddc
quantumakh <aadzhanow@gmail.com>
2024-10-11 13:40:33 +0200

    Revert "Update DestinationProtocol.swift"
    This reverts commit 1ddeae01cfd7158567cc222d036b3f9591f19e2c.

--- Sources/ModalPresentationKit/DestinationProtocol.swift
+++ Sources/ModalPresentationKit/DestinationProtocol.swift
@@ -5,22 +5,6 @@
 //  Created by Alisher on 09.10.2024.
 //
 
-// DestinationProtocol.swift
-
 import SwiftUI
 
-public protocol BaseDestinationProtocol: View, Identifiable, Hashable {}
-
-public extension BaseDestinationProtocol where Self: Equatable {
-    static func ==(lhs: Self, rhs: Self) -> Bool {
-        return lhs.id == rhs.id
-    }
-}
-
-public extension BaseDestinationProtocol {
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-}
-
-public typealias DestinationProtocol = BaseDestinationProtocol
+public typealias DestinationProtocol = View & Identifiable & Hashable
